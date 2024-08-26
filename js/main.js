4484b589b625e9e362a549d227ad0457c6db86c1
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-08-26 12:28:03 -0600

    agregando cambios en drag

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+    $("#draggable").draggable({
+        snap: true,
+
+    });
+    $("#draggable2").draggable({ snap: ".ui-widget-header" });
+    $("#draggable3").draggable({ snap: ".ui-widget-header" });
+    $("#draggable4").draggable({ snap: ".ui-widget-header" });
+    $("#draggable5").draggable({ snap: ".ui-widget-header" });
+    $("#draggable6").draggable({ snap: ".ui-widget-header" });
+    $("#draggable7").draggable({ snap: ".ui-widget-header" });
+    $("#draggable8").draggable({ snap: ".ui-widget-header" });
+    $("#draggable9").draggable({ snap: ".ui-widget-header" });
+    $("#draggable10").draggable({ snap: ".ui-widget-header" });
+    $("#draggable11").draggable({ snap: ".ui-widget-header" });
+    $("#draggable12").draggable({ snap: ".ui-widget-header" });
+    $("#draggable13").draggable({ snap: ".ui-widget-header" });
+    $("#draggable14").draggable({ snap: ".ui-widget-header" });
+    $("#draggable15").draggable({ snap: ".ui-widget-header" });
+    $("#draggable16").draggable({ snap: ".ui-widget-header" });
+    $("#draggable17").draggable({ snap: ".ui-widget-header" });
+    $("#draggable18").draggable({ snap: ".ui-widget-header" });
+    $("#draggable19").draggable({ snap: ".ui-widget-header" });
+    $("#draggable10").draggable({ snap: ".ui-widget-header" });
+    $("#draggable20").draggable({ snap: ".ui-widget-header" });
+    $("#draggable21").draggable({ snap: ".ui-widget-header" });
+    $("#draggable22").draggable({ snap: ".ui-widget-header" });
+    $("#draggable23").draggable({ snap: ".ui-widget-header" });
+    $("#draggable24").draggable({ snap: ".ui-widget-header" });
+    $("#draggable25").draggable({ snap: ".ui-widget-header" });
+    $("#draggable26").draggable({ snap: ".ui-widget-header" });
+    $("#draggable27").draggable({ snap: ".ui-widget-header" });
+    $("#draggable28").draggable({ snap: ".ui-widget-header" });
+    $("#draggable29").draggable({ snap: ".ui-widget-header" });
+    $("#draggable30").draggable({ snap: ".ui-widget-header" });
+});
